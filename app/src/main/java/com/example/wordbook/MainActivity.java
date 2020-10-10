e4945664b92bddccfffd986f4baa9c3d7417b3f0
Sen-Bac0n <55573584+Sen-Bac0n@users.noreply.github.com>
2020-10-10 13:10:52 +0800

    单词本

---
+++ app/src/main/java/com/example/wordbook/MainActivity.java
@@ -0,0 +1,278 @@
+package com.example.wordbook;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TableLayout;
+
+import com.example.wordbook.wordc.Words;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements WordListFragment.OnFragmentInteractionListener,WordDetailFragment.OnFragmentInteractionListener{
+    WordsDBHelper helper;
+
+    public static List<Activity> activityList = new LinkedList();
+    private static final String TAG = "text";
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.caidan,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch(item.getItemId()){
+            case R.id.all:{
+                RefreshWordItemFragment();
+                break;
+            }
+            case R.id.search:{
+                SearchDialog();
+                break;
+            }
+            case R.id.add:{
+                InsertDialog();
+                break;
+            }
+            case R.id.help:{
+                Intent intent = new Intent(MainActivity.this,Help.class);
+                startActivity(intent);
+                break;
+            }
+            case R.id.exit:{
+                exit();
+            }
+        }
+        return false;
+    }
+    public void exit(){
+
+        for(Activity act:activityList){
+
+            act.finish();
+
+        }
+
+        System.exit(0);
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        activityList.add(this);
+        helper = new WordsDBHelper(this);
+        helper.getReadableDatabase();
+        //SQLiteDatabase db= SQLiteDatabase.openOrCreateDatabase("mydb",null);
+
+
+        helper.close();
+
+    }
+
+    @Override
+    public void onWordItemClick(String id) {
+        if(isLand()){
+            ChangeWordDetailFragment(id);
+        }else{
+            Intent intent = new Intent(MainActivity.this,WordDetailActivity.class);
+            intent.putExtra(WordDetailFragment.ARG_ID,id);
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    public void onDeleteDialog(String strID) {
+        DeleteDialog(strID);
+    }
+
+    @Override
+    public void onUpdateDialog(String strID) {
+            OperationDB operationDB = OperationDB.getOperations();
+            if(operationDB != null && strID != null){
+                Words.WordDescription item = operationDB.getSingleWord(strID);
+                if(item != null)
+                    UpdateDialog(strID,item.word,item.meaning,item.sample);
+
+            }
+    }
+
+    @Override
+    public void OnWordDetailClick(Uri uri) {
+
+    }
+
+    private boolean isLand(){
+        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+            return true;
+        }
+        return false;
+    }
+
+
+    private void ChangeWordDetailFragment(String id){
+        Bundle arguments = new Bundle();
+        arguments.putString(WordDetailFragment.ARG_ID, id);
+
+        WordDetailFragment fragment = new WordDetailFragment();
+        fragment.setArguments(arguments);
+        getSupportFragmentManager().beginTransaction().replace(R.id.worddetail, fragment).commit();
+    }
+
+    //新增对话框
+    public void InsertDialog() {
+        final TableLayout tableLayout = (TableLayout) getLayoutInflater().inflate(R.layout.insert, null);
+        new AlertDialog.Builder(this)
+                .setTitle("新增单词")//标题
+                .setView(tableLayout)//设置视图
+                //确定按钮及其动作
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        String strWord = ((EditText) tableLayout.findViewById(R.id.txtWord)).getText().toString();
+                        String strMeaning = ((EditText) tableLayout.findViewById(R.id.txtMeaning)).getText().toString();
+                        String strSample = ((EditText) tableLayout.findViewById(R.id.txtSample)).getText().toString();
+
+                        Log.v("test",strWord+":"+strMeaning+":"+strSample);
+                        //既可以使用Sql语句插入，也可以使用使用insert方法插入
+                        // InsertUserSql(strWord, strMeaning, strSample);
+                        OperationDB wordsDB=OperationDB.getOperations();
+                        wordsDB.Insert(strWord, strMeaning, strSample);
+
+                        //单词已经插入到数据库，更新显示列表
+                        RefreshWordItemFragment();
+                    }
+                })
+                //取消按钮及其动作
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {           }
+                })
+                .create()//创建对话框
+                .show();//显示对话框
+    }
+
+    //删除对话框
+    private void DeleteDialog(final String strId) {
+        new AlertDialog.Builder(this).setTitle("删除单词")
+                .setMessage("是否真的删除单词?")
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                       public void onClick(DialogInterface dialogInterface, int i) {
+                        //既可以使用Sql语句删除，也可以使用使用delete方法删除
+                        OperationDB wordsDB=OperationDB.getOperations();
+                        wordsDB.Delete(strId);
+                        //单词已经删除，更新显示列表
+                        RefreshWordItemFragment();
+                    }
+                }).setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                }).create().show();
+    }
+
+    //修改对话框
+    private void UpdateDialog(final String strId, final String strWord, final String strMeaning, final String strSample) {
+        final TableLayout tableLayout = (TableLayout) getLayoutInflater().inflate(R.layout.insert, null);
+        ((EditText) tableLayout.findViewById(R.id.txtWord)).setText(strWord);
+        ((EditText) tableLayout.findViewById(R.id.txtMeaning)).setText(strMeaning);
+        ((EditText) tableLayout.findViewById(R.id.txtSample)).setText(strSample);
+        new AlertDialog.Builder(this).setTitle("修改单词")//标题
+            .setView(tableLayout)//设置视图
+        // 确定按钮及其动作
+            .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    String strNewWord = ((EditText) tableLayout.findViewById(R.id.txtWord)).getText().toString();
+
+                    String strNewMeaning = ((EditText) tableLayout.findViewById(R.id.txtMeaning)).getText().toString();
+                    String strNewSample = ((EditText) tableLayout.findViewById(R.id.txtSample)).getText().toString();
+                    //既可以使用Sql语句更新，也可以使用使用update方法更新
+                    OperationDB wordsDB=OperationDB.getOperations();
+                    wordsDB.UpdateUseSql(strId, strNewWord, strNewMeaning, strNewSample);
+                    //单词已经更新，更新显示列表
+                    RefreshWordItemFragment();
+                }
+
+    })
+    //取消按钮及其动作
+    .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialogInterface, int i) {
+
+        }
+    }).create()//创建对话框
+      .show();//显示对话框
+
+    }
+
+    //查找对话框
+    private void SearchDialog() {
+        final TableLayout tableLayout = (TableLayout) getLayoutInflater()
+                .inflate(R.layout.searchterm, null);
+        new AlertDialog.Builder(this)
+                .setTitle("查找单词")//标题
+                .setView(tableLayout)//设置视图
+                //确定按钮及其动作
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        String txtSearchWord = ((EditText) tableLayout.findViewById(R.id.searchWord))
+                                .getText().toString();
+
+                        //单词已经插入到数据库，更新显示列表
+                        RefreshWordItemFragment(txtSearchWord);
+                    }
+                })
+                //取消按钮及其动作
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                    }
+                })
+                .create()//创建对话框
+                .show();//显示对话框
+    }
+
+    /**
+     * 更新单词列表
+     */
+    private void RefreshWordItemFragment() {
+        WordListFragment wordItemFragment = (WordListFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.wordslist);
+        wordItemFragment.refreshWordsList();
+    }
+
+    /**
+     * 更新单词列表
+     */
+    private void RefreshWordItemFragment(String strWord) {
+        WordListFragment wordItemFragment = (WordListFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.wordslist);
+        wordItemFragment.refreshWordsList(strWord);
+
+    }
+
+
+
+}
+
---
+++ app/src/main/java/com/example/wordbook/OperationDB.java
@@ -0,0 +1,125 @@
+package com.example.wordbook;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.wordbook.wordc.Words;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class OperationDB {
+
+    static WordsDBHelper helper;
+
+    static OperationDB ins = new OperationDB();
+    public static OperationDB getOperations(){
+        return OperationDB.ins;
+    }
+
+    private OperationDB(){
+        if(helper == null){
+            helper = new WordsDBHelper(WordsApplication.getContext());
+        }
+    }
+
+    public void close(){
+        if(helper != null){
+            helper.close();
+        }
+    }
+
+    public Words.WordDescription getSingleWord(String id){
+        Words.WordDescription result = new Words.WordDescription();
+        SQLiteDatabase db = helper.getReadableDatabase();
+        String sql = "select * from words where _id = ?";
+        Cursor cursor = db.rawQuery(sql,new String[]{id});
+        if(cursor.moveToFirst()){
+            result.setWord(cursor.getString(cursor.getColumnIndex("word")));
+            result.setMeaning(cursor.getString(cursor.getColumnIndex("meaning")));
+            result.setSample(cursor.getString(cursor.getColumnIndex("sample")));
+            System.out.println("result: "+result.getWord());
+            return result;
+        }
+
+        return null;
+
+    }
+
+    public ArrayList<Map<String,String>> getAllWord(){
+        SQLiteDatabase db = helper.getReadableDatabase();
+        String sql = "select * from words ";
+        Cursor cursor = db.rawQuery(sql,null);
+        return ConvertCursorToWordList(cursor);
+    }
+
+    //疑难
+    public ArrayList<Map<String,String>> ConvertCursorToWordList(Cursor cursor){
+        ArrayList<Map<String,String>> list = new ArrayList<Map<String, String>>();
+        for (cursor.moveToFirst(); !cursor.isAfterLast(); cursor.moveToNext()) {
+            Map<String,String> map = new HashMap<String,String>();
+            System.out.println("dsl:"+cursor.getString(cursor.getColumnIndex("_id"))+ "word:"+cursor.getString(cursor.getColumnIndex("word")));
+            map.put(Words.Word._ID, cursor.getString(cursor.getColumnIndex("_id")));
+            map.put(Words.Word.COLUMN_NAME_WORD, cursor.getString(cursor.getColumnIndex("word")));
+            list.add(map);
+        }
+
+
+        return list;
+    }
+
+    public void InsertWord (String word, String meaning, String sample){
+        String sql="insert into  words(word,meaning,sample) values(?,?,?)";
+
+        SQLiteDatabase db = helper.getWritableDatabase();
+        db.execSQL(sql,new String[]{word,meaning,sample});
+    }
+
+    public void Insert(String word, String meaning, String sample){
+        String sql="insert into  words(word,meaning,sample) values(?,?,?)";
+
+        //Gets the data repository in write mode*/
+        SQLiteDatabase db = helper.getWritableDatabase();
+        db.execSQL(sql,new String[]{word,meaning,sample});
+    }
+
+
+    public void Delete(String strID){
+        String sql="delete from words where _id='"+strID+"'";
+
+        SQLiteDatabase db = helper.getReadableDatabase();
+        db.execSQL(sql);
+    }
+
+    public void UpdateUseSql(String strId, String strWord, String strMeaning, String strSample) {
+        SQLiteDatabase db = helper.getReadableDatabase();
+        String sql="update words set word=?,meaning=?,sample=? where _id=?";
+        db.execSQL(sql, new String[]{strWord, strMeaning, strSample,strId});
+    }
+    public void Update(String strId, String strWord, String strMeaning, String strSample) {
+        SQLiteDatabase db = helper.getReadableDatabase();
+        String sql="update words set word=?,meaning=?,sample=? where _id=?";
+        db.execSQL(sql, new String[]{strWord, strMeaning, strSample,strId});
+    }
+    //查找
+    public ArrayList<Map<String, String>> SearchUseSql(String strWordSearch) {
+        SQLiteDatabase db = helper.getReadableDatabase();
+
+        String sql="select * from words where word like ? order by word desc";
+        Cursor c=db.rawQuery(sql,new String[]{"%"+strWordSearch+"%"});
+
+        return ConvertCursorToWordList(c);
+    }
+    public ArrayList<Map<String, String>> Search(String strWordSearch) {
+        SQLiteDatabase db = helper.getReadableDatabase();
+
+        String sql="select * from words where word like ? order by word desc";
+        Cursor c=db.rawQuery(sql,new String[]{"%"+strWordSearch+"%"});
+
+        return ConvertCursorToWordList(c);
+    }
+
+
+
+}
